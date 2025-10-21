29be9fb0bb6ba6d4b44ab9cc818b6b06d69c3c4a
BaoBao <164650760+baongoc004@users.noreply.github.com>
2025-10-21 10:42:37 +0700

    Refactor bangcuuchuong to handle input directly
    Removed main function and integrated input into bangcuuchuong.

--- bai6.c
+++ bai6.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 int bangcuuchuong(int a) {
+    printf("Nhap bang cuu chuong: ");
+    scanf("%d", &a);
     for (int i = 1; i <= 10; i++) {
         printf("%d x %d = %d\n", a, i, a * i);
     }
-    return 0;
-}
-
-int main() {
-    int n;
-    printf("Nhap bang cuu chuong: ");
-    scanf("%d", &n);
-    bangcuuchuong(n);
     return 0;
 }
